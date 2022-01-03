de34e5cc378546bf0c00e759300786c4f86b1588
sirigg98 <91034206+sirigg98@users.noreply.github.com>
2022-01-03 16:40:41 -0500

    Matches USTR name used for search to the name of the result generated

--- OrbisAutomate.py
+++ OrbisAutomate.py
@@ -25,10 +25,9 @@ import selenium.webdriver.support.ui as ui
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.expected_conditions import _find_element
 #Selenium excpetions
-from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementClickInterceptedException
-
-from difflib import SequenceMatcher
+from selenium.common.exceptions import ElementNotInteractableException, NoSuchElementException, TimeoutException, ElementClickInterceptedException, StaleElementReferenceException
 #Misc
+from difflib import SequenceMatcher 
 import time
 import os
 from random import sample, seed
@@ -64,21 +63,24 @@ def save_firms(browser, svname):
 
 #Ugh so ugly. Is there a better waty to do this
 def export_df(browser):
-    wait = WebDriverWait(browser, 10)
-    action = ActionChains(browser)
+    wait = WebDriverWait(browser, 60)
     
     results = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"body > section.side-main > div > ul > li.results > a")))
+    time.sleep(5)
     results.click()
     
-    time.sleep(5)
-    addcols = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#main-content > div > div.results > div:nth-child(2) > a")))
-    addcols.click()
-
+    time.sleep(40)
+    try:
+        addcols = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#main-content > div > div.results > div:nth-child(2) > a")))
+        addcols.click()
+    except ElementClickInterceptedException:
+        addcols = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#main-content > div > div.results > div:nth-child(2) > a")))
+        addcols.click()
     ###########Total assets last avail
     #Financial data 
     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#main-content > div > div.format-editor-widget > div:nth-child(1) > div > div:nth-child(2) > div > ul > li:nth-child(6) > div"))).click()
     #Key financials
-    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#main-content > div > div.format-editor-widget > div:nth-child(1) > div > div:nth-child(2) > div > ul > li.with-nodes.opened.selected > ul > li:nth-child(2) > div"))).click()
+    wait.untilcd(EC.element_to_be_clickable((By.CSS_SELECTOR, "#main-content > div > div.format-editor-widget > div:nth-child(1) > div > div:nth-child(2) > div > ul > li.with-nodes.opened.selected > ul > li:nth-child(2) > div"))).click()
     #Total assets widget
     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#KEY_FINANCIALS\*KEY_FINANCIALS\.TOAS\:UNIVERSAL > div.text-container > span.px16.configuration.financialRepeat"))).click()
         
@@ -130,17 +132,54 @@ def export_df(browser):
     
     ###########Size classification
     #LEgal section
-    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#main-content > div > div.format-editor-widget > div:nth-child(1) > div > div:nth-child(2) > div > ul > li:nth-child(5) > div"))).click()
-    
+    try:
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#main-content > div > div.format-editor-widget > div:nth-child(1) > div > div:nth-child(2) > div > ul > li:nth-child(5) > div"))).click()
+    except ElementClickInterceptedException:
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#main-content > div > div.format-editor-widget > div:nth-child(1) > div > div:nth-child(2) > div > ul > li:nth-child(5) > div"))).click()
     #size classification
     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#LEGAL_ACCOUNT_INFORMATION\*LEGAL_ACCOUNT_INFORMATION\.COMPANY_CATEGORY\:UNIVERSAL > div.icon-container > span"))).click()
+    
+    ########### Industry Classification
+    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#main-content > div > div.format-editor-widget > div:nth-child(1) > div > div:nth-child(2) > div > ul > li:nth-child(3) > div"))).click()
+    time.sleep(2)
+    
+    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#main-content > div > div.format-editor-widget > div:nth-child(1) > div > div:nth-child(2) > div > ul > li.with-nodes.selected.opened > ul > li:nth-child(1) > div"))).click()
+    time.sleep(2)
+    naics = browser.find_element_by_css_selector("#INDUSTRY_ACTIVITIES\*INDUSTRY_ACTIVITIES\.NAICS2017_CORE_CODE\:UNIVERSAL > div.icon-container > span")
+    actions = ActionChains(browser)
+    actions.move_to_element(naics).perform()
+    naics.click()
+    
+    # try:
+    #     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#INDUSTRY_ACTIVITIES\*INDUSTRY_ACTIVITIES\.NAICS2017_CORE_CODE\:UNIVERSAL > div.icon-container > span"))).click()
+    # except TimeoutException:
+    #     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#INDUSTRY_ACTIVITIES\*INDUSTRY_ACTIVITIES\.NAICS2017_CORE_CODE\:UNIVERSAL > div.icon-container > span"))).click()
+
+
     
     ###########Addresses
     #contactinfo
     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#main-content > div > div.format-editor-widget > div:nth-child(1) > div > div:nth-child(2) > div > ul > li:nth-child(2) > div"))).click()
     
+
+
+    
+    #Postcode
+    try:
+        zipcode = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#CONTACT_INFORMATION\*CONTACT_INFORMATION\.POSTCODE\:UNIVERSAL > div.icon-container > span")))
+    except ElementClickInterceptedException:
+        zipcode = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#CONTACT_INFORMATION\*CONTACT_INFORMATION\.POSTCODE\:UNIVERSAL > div.icon-container > span")))
+    
+    actions = ActionChains(browser)
+    actions.move_to_element(zipcode).perform()
+    zipcode.click()
+    save = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > section.website > div:nth-child(6) > div.buttons.popup__buttons > a.button.ok")))
+    save.click()
+    
     #Address1
-    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#CONTACT_INFORMATION\*CONTACT_INFORMATION\.ADDRESS_LINE1\:UNIVERSAL > div.icon-container > span"))).click()
+    addy1 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#CONTACT_INFORMATION\*CONTACT_INFORMATION\.ADDRESS_LINE1\:UNIVERSAL > div.icon-container > span")))
+    actions.move_to_element(addy1).perform()
+    addy1.click()
     save = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > section.website > div:nth-child(6) > div.buttons.popup__buttons > a.button.ok")))
     save.click()
 
@@ -148,6 +187,7 @@ def export_df(browser):
     try:
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#CONTACT_INFORMATION\*CONTACT_INFORMATION\.ADDRESS_LINE2\:UNIVERSAL > div.icon-container > span"))).click()
     except ElementClickInterceptedException:    
+        time.sleep(2)
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#CONTACT_INFORMATION\*CONTACT_INFORMATION\.ADDRESS_LINE2\:UNIVERSAL > div.icon-container > span"))).click()
     save = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > section.website > div:nth-child(6) > div.buttons.popup__buttons > a.button.ok")))
     save.click()
@@ -162,17 +202,9 @@ def export_df(browser):
     
     #Address4
     try:
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#CONTACT_INFORMATION\*CONTACT_INFORMATION\.ADDRESS_LINE3\:UNIVERSAL > div.icon-container > span"))).click()
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#CONTACT_INFORMATION\*CONTACT_INFORMATION\.ADDRESS_LINE4\:UNIVERSAL > div.icon-container > span"))).click()
     except ElementClickInterceptedException:  
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#CONTACT_INFORMATION\*CONTACT_INFORMATION\.ADDRESS_LINE4\:UNIVERSAL > div.icon-container > span"))).click()
-    save = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > section.website > div:nth-child(6) > div.buttons.popup__buttons > a.button.ok")))
-    save.click()
-    
-    #Postcode
-    try:
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#CONTACT_INFORMATION\*CONTACT_INFORMATION\.POSTCODE\:UNIVERSAL > div.icon-container > span"))).click()
-    except ElementClickInterceptedException:
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#CONTACT_INFORMATION\*CONTACT_INFORMATION\.POSTCODE\:UNIVERSAL > div.icon-container > span"))).click()
     save = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > section.website > div:nth-child(6) > div.buttons.popup__buttons > a.button.ok")))
     save.click()
     
@@ -238,43 +270,48 @@ def orbis_auto(browser, ls):
     # time.sleep(5)
 
     ##################################################################################
-    missed_firms = []
+    matched_firms = []
     ##################################################################################
     #Cycle through firm names, add to selection
-    for firm in ls:  
-
-        if browser.current_url.endswith('Report'):
-            revsearch = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"body > section.side-main > div > ul > li.search > a")))
-            revsearch.click()
-        else:
-            browser.refresh()
-        
-        print(ls.index(firm))
-        #Every 150 firms, manually go and download data. The webpage cannot handle ~200 or more queries in one go i think.
-        if ls.index(firm)%100 == 0 and ls.index(firm) != 0:  
-            export_df(browser)
-            print(f"{ls.index(firm)} firms completed. File exported.")
-            # save_firms(browser, svname)
-            revsearch = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"body > section.side-main > div > ul > li.search > a")))
-            revsearch.click()
+    
+    for firm in ls:   
             
-        
+        browser.refresh()
         orbissearch = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input#search")))
-        orbissearch.send_keys(f"{firm}, United States of America")
+        orbissearch.send_keys(f"{firm[1]}, United States of America")
         orbissearch.send_keys(Keys.ENTER)
+        
+    
+        print(ls.index(firm))
         # If pop-up shows up to add new search to current selection of erase current selection
         #Search for firm name, if no firm with name then move to next element in list
         try:    
             firstresult = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#quicksearch-results > ul > li:nth-child(1)")))
+            orbisname = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#quicksearch-results > ul > li:nth-child(1) > a > div.column.column-flex > div.column.column-name > p.name"))).text
+            matched_firms.append([orbisname, firm[0], firm[1]])
             firstresult.click()
+            
         #If no search matches, continue to next name on list
         except TimeoutException:
-            missed_firms.append(firm)
+            matched_firms.append(["", firm[0], firm[1]])
             continue
-        time.sleep(3)
-
-    
-    return missed_firms
+        
+        if ls.index(firm)%100 == 0 or ls.index(firm) == len(ls)-1:
+            if ls.index(firm) != 0:
+                print('Exporting...')
+                export_df(browser)
+                print(f"{ls.index(firm)} firms completed. File exported.")
+            # save_firms(browser, svname)
+                revsearch = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"body > section.side-main > div > ul > li.search > a")))
+                revsearch.click()
+        
+        time.sleep(3)    
+            
+        if "report" in browser.current_url.lower():
+            revsearch = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"body > section.side-main > div > ul > li.search > a")))
+            revsearch.click()
+            time.sleep(3)
+    return matched_firms
 
 def replace_all(text, dic):
     for i, j in dic.items():
@@ -292,85 +329,86 @@ if __name__ == "__main__":
     firms4 = list(pd.read_csv(r"C:\Users\F0064WK\OneDrive - Tuck School of Business at Dartmouth College\Documents\US Tariff Exemptions\USTR data\Check_List4.csv").iloc[:,1])
     
    
-    temp = [x for x in firms4 if x != "E  E CO LTD DBA JLA HOME"]
-    firms4_ls= []
-    match = re.compile("LLC.|LLC|llc.|llc|LTD.|LTD|Ltd.|Ltd|ltd.|ltd|INC.|INC|Inc.|Inc|inc.|inc|Corp.|Corp|Corporation|Company|Companies|Co")
-    match_dict = {"LLC.":"",
-                  "LLC": "",
-                  "llc.": "",
-                  "llc": "",
-                  "LTD.": "",
-                  "LTD":"",
-                  "Ltd.": "",
-                  "Ltd":"",
-                  "ltd.": "",
-                  "ltd": "",
-                  "INC.": "",
-                  "INC": "",
-                  "Inc.":"",
-                  "Inc":"",
-                  "inc.":"",
-                  "inc":"",
-                  "Incorporated": "",
-                  "incorporated":"",
-                  "corp.":"",
-                  "corp":"",
-                  "CORP": "",
-                  "corporation":"",
-                  "Company":"",
-                  "Companies":"",
-                  "Co":"",
-                  "co": "",
+    temp = [x for x in firms1 if x != "E  E CO LTD DBA JLA HOME"]
+    temp = list(set(temp))
+    firms3_ls= []
+    match = re.compile("llc.|llc|ltd.|ltd|inc.|inc|corp.|corporation|corp|company|companies|co.")
+    match_dict = {" llc.": "",
+                  " llc": "",
+                  " ltd.": "",
+                  " ltd": "",
+                  " limited": "",
+                  " incorporated":"",
+                  " inc.":"",
+                  " inc":"",
+                  " corp.":"",
+                  " corporation":"",
+                  " corp":"",
+                  " company":"",
+                  " companies":"",
+                  " co.": "",
+                  " co": "",
                   ",": "", 
                   "united states of america": "",
-                  "usa": "", "(":"", ")": "", "u.s.a":""}
+                  "usa": "", 
+                  "(":"", 
+                  ")": "", 
+                  " u.s.a":"", 
+                  " +":"", 
+                  " america" : ""}
     
     firmnames = []
     for i in temp:
+        if pd.isna(i):
+            i = ''
+        if "Continental" in i:
+            firmnames.append([i, i.replace(",", "").replace(".", "")])
+            continue
         try:
-            pattern_split = re.split(match, i)
+            pattern_split = re.split(match, i.lower())
         except TypeError:
             continue
-        l = len(pattern_split[0].split())
-        i_ls = i.split()
+        l = len(pattern_split[0].replace(",", "").replace(".", "").split())
+        i_ls = i.replace(",", "").replace(".", "").split()
+        if "dba" in i:
+            i_split = i.split("dba")
+            i = i_split[0]
+            continue
+        if "d/b/a" in i:
+            i_split = i.split("d/b/a")
+            i = i_split[0]
+            continue
         try:
-            firmnames.append([i, pattern_split[0].replace(",", "") + i_ls[l]])
+            firmnames.append([i, pattern_split[0] + i_ls[int(l+1)]])
         except IndexError:
             firmnames.append([i, i])
             
-    # firms3_ls = list(set(firms3_ls))
+    firmnames = [x for x in firmnames if firmnames.index(x)> 302]
+    # firms_orbis = list(pd.read_csv(r"C:\Users\F0064WK\Downloads\CheckList2_firmsize_address.csv").iloc[:,0])
+    # matched_names = []
     
-    
+    # for i in USTRfirms:
+    #     print(USTRfirms.index(i))
+    #     orbisname = replace_all(i[0].lower(), match_dict)
+    #     for j in firms_orbis:
+    #         USTRname = replace_all(j.lower(), match_dict)
+    #         USTRname_ls = USTRname.split()
+    #         ratio = SequenceMatcher(None, orbisname, USTRname).ratio()
+    #         matched_names.append([j, i[0], i[1] + ", United States of America", ratio])
+    #         if ratio == 1:
+    #             firms_orbis.remove(j)
         
-    
-    firms_orbis = list(pd.read_csv(r"C:\Users\F0064WK\Downloads\CheckList4_firmsize_address.csv").iloc[:,1])
-    matched_names = []
-    
-    for i in firms_orbis:
-        print(firms_orbis.index(i))
-        orbisname = replace_all(i.lower(), match_dict)
-        for j in firmnames:
-            USTRname = replace_all(j[0].lower(), match_dict)
-            USTRname_ls = USTRname.split()
-            ratio = SequenceMatcher(None, orbisname, USTRname).ratio()
-            matched_names.append([i, j[0], j[1] + ", United States of America", ratio])
-            if ratio == 1:
-                break
-    match_names_df_temp = pd.DataFrame(matched_names, columns = ["Orbis Name", "USTR Name", "Search Term", "Fuzzy_Ratio"])
-    match_names_df = match_names_df_temp.sort_values("Fuzzy_Ratio", ascending = False).drop_duplicates("Orbis Name").set_index("Orbis Name")
+        
+    # match_names_df_temp = pd.DataFrame(matched_names, columns = ["Orbis Name", "USTR Name", "Search Term", "Fuzzy_Ratio"])
+    # match_names_df = match_names_df_temp.sort_values("Fuzzy_Ratio", ascending = False).drop_duplicates("Orbis Name").set_index("Orbis Name")
 
-    match_names_df.to_csv(r"C:\Users\F0064WK\Downloads\names4.csv")
-# # ls = [x for x in firms3_2_ls if firms3_2_ls.index(x) > 159
-
-#     # browser = openbrowser()
-#     # missed_firms2 =  orbis_auto(browser, ls)
-     
-#     #export_df(browser)
+    # match_names_df.to_csv(r"C:\Users\F0064WK\Downloads\names2.csv")
+    # ls = [x for x in firms3_2_ls if firms3_2_ls.index(x) > 159
     
-    
-    
-    
-    
-    
-    
-    
+    browser = openbrowser()
+    # matched_firms =  orbis_auto(browser, firmnames)
+    # for i in matched_firms:
+    #     i.append(SequenceMatcher(None, i[1].lower(), i[0].lower()).ratio())
+    # df = pd.DataFrame(matched_firms, columns=['orbis_name', 'ustr_name', 'search_term', 'match_ratio'])
+    # df.to_csv(r'C:\Users\F0064WK\Downloads\crosswalk_1.csv')
+    # #export_df(browser)
